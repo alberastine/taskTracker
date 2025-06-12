5ac2cac66344e551383ad3921b4bb469c780fdef
alberastine <126144757+alberastine@users.noreply.github.com>
2025-06-12 15:43:21 +0800

    base url update

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:5000/api/users',
+  // baseURL: 'http://localhost:5000/api/users',
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
