4e9c020bbc7b740225b7fddd4d165d1eb2cfce1a
alberastine <126144757+alberastine@users.noreply.github.com>
2025-06-12 19:13:33 +0800

    bug fix 1

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export default axios.create({
   // baseURL: 'http://localhost:5000/api/users',
-  baseURL: 'https://tasktrackerapi-9nfn.onrender.com/api/users',
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
