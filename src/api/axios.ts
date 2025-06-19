42be85fc17cb7abe04dc3c4d28cb47e3bb0bbd50
alberastine <126144757+alberastine@users.noreply.github.com>
2025-06-19 10:47:16 +0800

    deploy test 4.2 use the url in env

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 export default axios.create({
   // baseURL: 'http://localhost:5000/api/users',
-  baseURL: 'https://tasktrackerapi-lzeh.onrender.com/api/users',
+  // baseURL: 'https://tasktrackerapi-lzeh.onrender.com/api/users',
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
